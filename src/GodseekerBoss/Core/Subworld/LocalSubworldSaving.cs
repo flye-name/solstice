24241832d1ad0feb650b4dbe43a59eb36e2def60
Zoe <88523932+ZenTheMod@users.noreply.github.com>
2026-03-13 17:54:27 -0700

    clean up subworld local saving

---
+++ src/GodseekerBoss/Core/Subworld/LocalSubworldSaving.cs
@@ -0,0 +1,65 @@
+﻿using Daybreak.Common.Features.Hooks;
+using GodseekerBoss.Content.Aerie.Environment;
+using MonoMod.Cil;
+using SubworldLibrary;
+using System.IO;
+using System.Reflection;
+using Terraria.IO;
+using Terraria.ModLoader;
+
+namespace GodseekerBoss.Core.Subworld;
+
+#if DEBUG
+internal static class LocalSubworldSaving
+{
+    [OnLoad]
+    private static void Load()
+    {
+        MonoModHooks.Modify(
+            typeof(SubworldSystem).GetMethod(
+                nameof(SubworldSystem.LoadWorld),
+                BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Public
+            ),
+            LoadWorld_LoadLocalFile
+        );
+    }
+
+    private static void LoadWorld_LoadLocalFile(ILContext il)
+    {
+        ILCursor c = new(il);
+
+        int inSubworld_varNum = -1;
+        int path_varNum = -1;
+
+        if (
+            !c.TryGotoNext(
+                MoveType.After,
+                i => i.MatchLdloc(out inSubworld_varNum),
+                i => i.MatchBrtrue(out _),
+                i => i.MatchLdsfld<SubworldSystem>(nameof(SubworldSystem.main)),
+                i => i.MatchCallvirt<FileData>($"get_{nameof(FileData.Path)}"),
+                i => i.MatchBr(out _),
+                i => i.MatchCall<SubworldSystem>($"get_{nameof(SubworldSystem.CurrentPath)}"),
+                i => i.MatchStloc(out path_varNum)
+            )
+        )
+        {
+            ModContent.GetInstance<GodseekerBoss>().Logger.Debug("GodSeeker: Could not find Subworld Library's subworld file path variable!");
+            return;
+        }
+
+        c.EmitLdloca(path_varNum);
+        c.EmitLdloc(inSubworld_varNum);
+
+        c.EmitDelegate(
+            static (ref string path, bool flag) =>
+            {
+                if (SubworldSystem.current is AerieSubworld && flag)
+                {
+                    path = Path.Combine(ModContent.GetInstance<GodseekerBoss>().SourceFolder, "Subworld_Save", SubworldSystem.current.FileName + ".wld");
+                }
+            }
+        );
+    }
+}
+#endif
