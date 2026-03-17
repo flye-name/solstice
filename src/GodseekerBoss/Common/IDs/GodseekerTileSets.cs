4751aa8c67c340658b7474950d954fd966d7742d
Zoe <88523932+ZenTheMod@users.noreply.github.com>
2026-03-17 01:20:44 -0700

    restructure grass tiles

---
+++ src/GodseekerBoss/Common/IDs/GodseekerTileSets.cs
@@ -0,0 +1,108 @@
+﻿using Daybreak.Common.Features.Hooks;
+using MonoMod.Cil;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace GodseekerBoss.Common.IDs;
+
+public static class GodseekerTileSets
+{
+    public static bool[] UseAlternateTileObjectDataRandomStyles { get; private set; } = [];
+
+    private static Mod Mod => ModContent.GetInstance<GodseekerBoss>();
+
+    [ModSystemHooks.ResizeArrays]
+    private static void ResizeArrays()
+    {
+        UseAlternateTileObjectDataRandomStyles = CreateSet<bool>(nameof(UseAlternateTileObjectDataRandomStyles), false);
+
+        return;
+
+        static T[] CreateSet<T>(string name, T defaultState)
+        {
+            return TileID.Sets.Factory.CreateNamedSet(Mod, name)
+                         .RegisterCustomSet(defaultState);
+        }
+    }
+
+    [OnLoad]
+    private static void Load()
+    {
+        // CanPlace does not account for alternate TileObjectData's RandomStyleRange nor
+        // SpecificRandomStyles -- although irrelevant since this tile does not use it --
+        // unlike GetTileData(Tile) which does for some unknown reason.
+        // Additionally, I cannot understand the logic TileObject.CanPlace uses to find
+        // the tiles alternate TileObjectData, so I use my own (this is a hack, I am not
+        // nearly talented enough to correctly do this.)
+        IL_TileObject.CanPlace += CanPlace_UseCorrectTileObjectDataForRandomStyles;
+    }
+
+    private static void CanPlace_UseCorrectTileObjectDataForRandomStyles(ILContext il)
+    {
+        var c = new ILCursor(il);
+
+        int typeIndex = -1; // arg
+        int styleIndex = -1; // arg
+
+        int tileDataIndex = -1; // loc
+
+        c.GotoNext(
+            i => i.MatchCall<TileObjectData>(nameof(TileObjectData.GetTileData)),
+            i => i.MatchStloc(out tileDataIndex)
+        );
+
+        c.GotoPrev(
+            i => i.MatchLdarg(out typeIndex), // type
+            i => i.MatchLdarg(out styleIndex) // style
+        );
+
+        while (c.TryGotoNext(
+                   MoveType.After,
+                   i => i.MatchLdloc(tileDataIndex),
+                   i => i.MatchCallvirt<TileObjectData>($"get_{nameof(TileObjectData.RandomStyleRange)}")
+               ))
+        {
+            c.EmitLdarg(typeIndex);
+            c.EmitLdarg(styleIndex);
+
+            c.EmitDelegate(
+                static (int randomStyleRange, int type, int style) =>
+                {
+                    if (!UseAlternateTileObjectDataRandomStyles[type])
+                    {
+                        return randomStyleRange;
+                    }
+
+                    TileObjectData alternateTileData = GetAlternateTileObjectData(type, style);
+
+                    return alternateTileData.RandomStyleRange;
+                }
+            );
+        }
+    }
+
+    private static TileObjectData GetAlternateTileObjectData(int type, int style)
+    {
+        TileObjectData tileObjectData = TileObjectData._data[type];
+
+        // TODO: SubTile support (no)
+
+        if (tileObjectData._alternates == null)
+        {
+            return tileObjectData;
+        }
+
+        foreach (TileObjectData data in tileObjectData.Alternates)
+        {
+            if (data != null &&
+                style >= data.Style &&
+                style <= data.Style + data.RandomStyleRange)
+            {
+                return data;
+            }
+        }
+        return tileObjectData;
+    }
+}
--- src/GodseekerBoss/Content/Aerie/Placements/AerieGrasses.cs
+++ src/GodseekerBoss/Content/Aerie/Placements/AerieGrasses.cs
@@ -1,7 +1,5 @@
-﻿using Daybreak.Common.Features.Hooks;
+﻿using GodseekerBoss.Common.IDs;
 using Microsoft.Xna.Framework;
-using MonoMod.Cil;
-using System;
 using System.Collections.Generic;
 using Terraria;
 using Terraria.Audio;
@@ -11,6 +9,7 @@ using Terraria.GameContent.Creative;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.ObjectData;
+// ReSharper disable InconsistentNaming
 
 namespace GodseekerBoss.Content.Aerie.Placements;
 
@@ -58,7 +57,7 @@ public class AerieGrassSeeds : ModItem
     }
 }
 
-public class AerieTallGrassSeeds : ModItem
+public class TallAerieGrassSeeds : ModItem
 {
     public override void SetStaticDefaults()
     {
@@ -67,7 +66,7 @@ public class AerieTallGrassSeeds : ModItem
 
     public override void SetDefaults()
     {
-        Item.DefaultToPlaceableTile(ModContent.TileType<AerieTallGrassTile>());
+        Item.DefaultToPlaceableTile(ModContent.TileType<Tall1x1AerieGrassTile>());
         Item.value = Item.sellPrice(silver: 5);
         // Vanilla flower seeds don't have auto-reuse.
         Item.autoReuse = false;
@@ -75,7 +74,7 @@ public class AerieTallGrassSeeds : ModItem
     }
 }
 
-public sealed class AerieTallGrassFlowerSeeds : AerieTallGrassSeeds
+public sealed class TallAerieGrassFlowerSeeds : TallAerieGrassSeeds
 {
     public override void SetDefaults()
     {
@@ -85,86 +84,9 @@ public sealed class AerieTallGrassFlowerSeeds : AerieTallGrassSeeds
     }
 }
 
-public sealed class AerieTallGrassTile : ModTile
+public sealed class Tall1x1AerieGrassTile : ModTile
 {
-    [OnLoad]
-    private static new void Load()
-    {
-        // CanPlace does not account for alternate TileObjectData's RandomStyleRange nor
-        // SpecificRandomStyles -- although irrelevant since this tile does not use it --
-        // unlike GetTileData(Tile) which does for some unknown reason.
-        // Additionally, I cannot understand the logic TileObject.CanPlace uses to find
-        // the tiles alternate TileObjectData, so I use my own (this is a hack, I am not
-        // nearly talented enough to correctly do this.)
-        IL_TileObject.CanPlace += CanPlace_UseCorrectTileObjectDataForRandomStyles;
-    }
-
-    private static void CanPlace_UseCorrectTileObjectDataForRandomStyles(ILContext il)
-    {
-        var c = new ILCursor(il);
-
-        int typeIndex = -1; // arg
-        int styleIndex = -1; // arg
-
-        int tileDataIndex = -1; // loc
-
-        c.GotoNext(
-            i => i.MatchCall<TileObjectData>(nameof(TileObjectData.GetTileData)),
-            i => i.MatchStloc(out tileDataIndex)
-        );
-
-        c.GotoPrev(
-            i => i.MatchLdarg(out typeIndex), // type
-            i => i.MatchLdarg(out styleIndex) // style
-        );
-
-        while (c.TryGotoNext(
-                   MoveType.After,
-                   i => i.MatchLdloc(tileDataIndex),
-                   i => i.MatchCallvirt<TileObjectData>($"get_{nameof(TileObjectData.RandomStyleRange)}")
-               ))
-        {
-            c.EmitLdarg(typeIndex);
-            c.EmitLdarg(styleIndex);
-
-            c.EmitDelegate(
-                static (int randomStyleRange, int type, int style) =>
-                {
-                    if (type != ModContent.TileType<AerieTallGrassTile>())
-                    {
-                        return randomStyleRange;
-                    }
-
-                    TileObjectData alternateTileData = GetAlternateTileObjectData(type, style);
-
-                    return alternateTileData.RandomStyleRange;
-                }
-            );
-        }
-    }
-
-    private static TileObjectData GetAlternateTileObjectData(int type, int style)
-    {
-        TileObjectData tileObjectData = TileObjectData._data[type];
-
-        // TODO: SubTile support (no)
-
-        if (tileObjectData._alternates == null)
-        {
-            return tileObjectData;
-        }
-
-        foreach (TileObjectData data in tileObjectData.Alternates)
-        {
-            if (data != null &&
-                style >= data.Style &&
-                style <= data.Style + data.RandomStyleRange)
-            {
-                return data;
-            }
-        }
-        return tileObjectData;
-    }
+    public override string Texture => GeneratedAssets.Content.Aerie.Placements.Textures.TallAerieGrassTile.Key;
 
     public override void SetStaticDefaults()
     {
@@ -198,6 +120,8 @@ public sealed class AerieTallGrassTile : ModTile
         TileID.Sets.DrawFlipMode[Type] = 1;
         TileID.Sets.IgnoredByGrowingSaplings[Type] = true;
 
+        GodseekerTileSets.UseAlternateTileObjectDataRandomStyles[Type] = true;
+
         AddMapEntry(new Color(185, 168, 72));
         // DustType = ModContent.DustType<AerieGrassDust>();
         HitSound = SoundID.Grass;
@@ -217,3 +141,54 @@ public sealed class AerieTallGrassTile : ModTile
         TileObject.CanPlace(i, j, Type, item.placeStyle, Main.LocalPlayer.direction, out _, onlyCheck: true);
     }
 }
+
+public sealed class Tall1x2AerieGrassTile : ModTile
+{
+    public override string Texture => GeneratedAssets.Content.Aerie.Placements.Textures.TallerAerieGrassTile.Key;
+
+    public override void SetStaticDefaults()
+    {
+        RegisterItemDrop(0, 0, 1);
+
+        Main.tileFrameImportant[Type] = true;
+        Main.tileCut[Type] = true;
+        Main.tileSolid[Type] = false;
+        Main.tileNoAttach[Type] = true;
+        Main.tileNoFail[Type] = true;
+        Main.tileLavaDeath[Type] = true;
+
+        TileObjectData.newTile.CopyFrom(TileObjectData.Style1x2);
+        TileObjectData.newTile.CoordinateHeights = [16, 18];
+        TileObjectData.newTile.LavaDeath = true;
+        TileObjectData.newTile.StyleHorizontal = true;
+        TileObjectData.newTile.RandomStyleRange = 4;
+        TileObjectData.newTile.StyleMultiplier = 4;
+
+        TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
+        TileObjectData.newAlternate.RandomStyleRange = 4;
+        TileObjectData.newAlternate.StyleMultiplier = 4;
+        TileObjectData.addAlternate(1);
+
+        TileObjectData.addTile(Type);
+
+        TileID.Sets.TileCutIgnore.Regrowth[Type] = true;
+        TileID.Sets.ReplaceTileBreakUp[Type] = true;
+        TileID.Sets.SlowlyDiesInWater[Type] = true;
+        TileID.Sets.SwaysInWindBasic[Type] = true;
+        TileID.Sets.DrawFlipMode[Type] = 1;
+        TileID.Sets.IgnoredByGrowingSaplings[Type] = true;
+
+        GodseekerTileSets.UseAlternateTileObjectDataRandomStyles[Type] = true;
+
+        AddMapEntry(new Color(185, 168, 72));
+        // DustType = ModContent.DustType<AerieGrassDust>();
+        HitSound = SoundID.Grass;
+    }
+
+    public override bool TileFrame(int i, int j, ref bool resetFrame, ref bool noBreak)
+    {
+        WorldGen.PlantCheck(i, j);
+        return false;
+    }
+}
+
