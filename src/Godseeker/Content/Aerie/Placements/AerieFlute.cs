f8b2f4016bc6032827c46014cf7a958f2cd3dae0
yoyocattus <may0naisecattus@gmail.com>
2026-04-03 10:52:18 +0100

    aerie flute held sprite and sound

--- src/Godseeker/Content/Aerie/Placements/AerieFlute.cs
+++ src/Godseeker/Content/Aerie/Placements/AerieFlute.cs
@@ -1,6 +1,8 @@
 ﻿using Godseeker.Content.Aerie.Environment;
+using Microsoft.Xna.Framework;
 using SubworldLibrary;
 using Terraria;
+using Terraria.Audio;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -14,10 +16,12 @@ public sealed class AerieFlute : ModItem
     {
         Item.width = 28;
         Item.height = 36;
-        Item.useAnimation = 30;
-        Item.useTime = 30;
-        Item.useStyle = ItemUseStyleID.HoldUp;
+        Item.useAnimation = 60;
+        Item.useTime = 60;
+        Item.useStyle = ItemUseStyleID.Shoot;
         Item.noUseGraphic = true;
+        Item.shoot = ModContent.ProjectileType<HeldFlute>();
+        Item.shootSpeed = 0f;
     }
 
     public override void ModifyResearchSorting(ref ContentSamples.CreativeHelper.ItemGroup itemGroup)
@@ -35,5 +39,45 @@ public sealed class AerieFlute : ModItem
         SubworldLoading.EnterAerie();
 
         return true;
+    }
+}
+
+public class HeldFlute : ModProjectile 
+{
+    public override string Texture => GeneratedAssets.Assets.Images.Textures.AerieFluteHeld.Key;
+    
+    public override void SetDefaults()
+    {
+        Projectile.width = 30;
+        Projectile.height = 8;
+        Projectile.friendly = true;
+        Projectile.tileCollide = false;
+        Projectile.penetrate = -1;
+    }
+
+    public override void AI()
+    {
+        Player player = Main.player[Projectile.owner];
+        if (Projectile.localAI[0] == 0)
+        {
+            SoundEngine.PlaySound(new SoundStyle("Godseeker/Assets/Flute"), Projectile.Center);
+            Projectile.localAI[0] = 1;
+        }
+        
+        Projectile.timeLeft = 2;
+
+        if (player.itemAnimation <= 0)
+        {
+            Projectile.Kill();
+        }
+        
+        Vector2 playerCenter = player.RotatedRelativePoint(player.MountedCenter);
+        
+        Projectile.Center = playerCenter + new Vector2(player.direction * 10, -3);
+        player.heldProj = Projectile.whoAmI;
+        player.SetDummyItemTime(2);
+        
+        Projectile.direction = player.direction;
+        Projectile.spriteDirection = player.direction;
     }
 }
