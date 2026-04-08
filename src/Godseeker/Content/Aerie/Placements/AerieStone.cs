dac55f16d8e4fbfc04d35b762171ef4835ae47f6
yoyocattus <may0naisecattus@gmail.com>
2026-04-08 11:05:36 +0100

    aerie stone placeholder

---
+++ src/Godseeker/Content/Aerie/Placements/AerieStone.cs
@@ -0,0 +1,51 @@
+﻿using Godseeker.Common.IDs;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+using PlacementTextures = Godseeker.GeneratedAssets.Assets.Images.Aerie.Placements.Textures;
+
+namespace Godseeker.Content.Aerie.Placements;
+
+public sealed class AerieStone : ModItem
+{
+    public override string Texture => PlacementTextures.AerieStone.Key;
+
+    public override void SetStaticDefaults()
+    {
+        CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 100;
+    }
+
+    public override void SetDefaults()
+    {
+        Item.DefaultToPlaceableTile(ModContent.TileType<AerieStoneTile>());
+    }
+
+    public override void AddRecipes()
+    {
+        
+    }
+}
+
+public class AerieStoneTile : ModTile
+{
+    public override string Texture => PlacementTextures.AerieStoneTile.Key;
+
+    public override void SetStaticDefaults()
+    {
+        Main.tileSolid[Type] = true;
+        Main.tileMergeDirt[Type] = true;
+        Main.tileBlockLight[Type] = true;
+        Main.tileLighted[Type] = false;
+        TileID.Sets.ChecksForMerge[Type] = true;
+
+        AddMapEntry(new Color(147, 144, 131));
+
+        DustType = -1;
+        HitSound = SoundID.Tink;
+    }
+}
