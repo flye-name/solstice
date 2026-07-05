a11951ff238c213df8679e1781d311e8c95c1421
flye-name <73665110+flye-name@users.noreply.github.com>
2026-07-05 23:50:31 +0200

    life simply isn't fair sometimes

--- src/Solstice/Content/Aerie/Visual/FlameParticles.cs
+++ src/Solstice/Content/Aerie/Visual/FlameParticles.cs
@@ -16,13 +16,11 @@ namespace Solstice.Content.Aerie;
 public static class FlameParticles
 {
     #region data
-    private const int flame_count = 600;
+    private const int flame_count = 1800;
     
     private sealed class Data : IStatic<Data>
     {
         public required WrapperShaderData<Assets.Effects.Flame.Parameters> FlameShader { get; init; }
-        public required VertexBuffer VertexBuffer { get; init; }
-        public required IndexBuffer IndexBuffer { get; init; }
 
         public static Data LoadData(Mod mod)
         {
@@ -30,21 +28,11 @@ public static class FlameParticles
                 () => new Data
                 {
                     FlameShader = Assets.Effects.Flame.CreateFlameShader(),
-                    
-                    VertexBuffer = new DynamicVertexBuffer(Main.graphics.GraphicsDevice, typeof(VertexPositionColorTexture), flame_count * 2, BufferUsage.None),
-                    IndexBuffer = new DynamicIndexBuffer(Main.graphics.GraphicsDevice, typeof(int), flame_count * 6, BufferUsage.None)
                 }
             ).GetAwaiter().GetResult();
         }
 
-        public static void UnloadData(Data data)
-        {
-            Main.RunOnMainThread(() =>
-            {
-                Data.Instance.VertexBuffer?.Dispose();
-                Data.Instance.IndexBuffer?.Dispose();
-            }).GetAwaiter().GetResult();
-        }
+        public static void UnloadData(Data data) { }
     }
     
     private record struct Flame(int Seed)
@@ -109,22 +97,21 @@ public static class FlameParticles
     #endregion
 
     #region rendering
-    private static readonly Vector2[] quadTexCoords = [
-        Vector2.Zero,
-        new(0, 1),
-        new(1, 0),
-        Vector2.One
-    ];
-            
-    private static readonly Vector2[] quadOrigins = [
-        -Vector2.One,
-        new(-1, 1),
-        new(1, -1),
-        Vector2.One
-    ];
-    
-    private static void GenerateFlameVertexData(List<int> indices, List<VertexPositionColorTexture> vertices, Vector2 baseOrigin)
+    public static void DrawFlames(SpriteBatch sb)
     {
+        var texture = Assets.Images.Bloom.Asset.Value;
+        var textureNoise = Assets.Images.NavierNoise.Asset.Value;
+        var origin = texture.Size() / 2f;
+        
+        sb.End(out var ss);
+
+        var shader = Data.Instance.FlameShader;
+        shader.Apply();
+        
+        sb.Begin(ss with { CustomEffect = shader.Shader, SamplerState = SamplerState.PointWrap });
+
+        Main.graphics.GraphicsDevice.Textures[1] = textureNoise;
+        
         foreach (var flame in flames)
         {
             if (flame.Progress <= 0f)
@@ -133,56 +120,19 @@ public static class FlameParticles
             var rand = new UnifiedRandom(flame.Seed);
             
             var position = flame.Position - Main.screenPosition;
-            var scale = flame.Progress * rand.NextFloat(0.1f, 0.25f);
+            var scale = Vector2.One * flame.Progress * rand.NextFloat(0.1f, 0.25f);
             
             var color = Color.Orange * flame.Progress;
             color.A = 0;
             
-            var newVertices = new VertexPositionColorTexture[4];
-            
-            for (int i = 0; i < 4; i++)
-                newVertices[i] = new(new Vector3(baseOrigin * quadOrigins[i] * scale + position, 0), color, quadTexCoords[i]);
-            
-            var start = vertices.Count;
-            int[] newIndices = [ start, start + 1, start + 2, start + 3, start + 2, start + 1 ];
-            
-            vertices.AddRange(newVertices);
-            indices.AddRange(newIndices);
+            sb.Draw(new DrawParameters(texture)
+            {
+                Position = position,
+                Origin = origin,
+                Color = color,
+                Scale = scale
+            });
         }
-    }
-    
-    public static void DrawFlames(SpriteBatch sb)
-    {
-        var texture = Assets.Images.Bloom.Asset.Value;
-        var textureNoise = Assets.Images.NavierNoise.Asset.Value;
-
-        var indices = new List<int>();
-        var vertices = new List<VertexPositionColorTexture>();
-        
-        GenerateFlameVertexData(indices, vertices, texture.Size() / 2f);
-
-        if (vertices.Count < 3)
-            return;
-        
-        sb.End(out var ss);
-        sb.Begin(ss with { SortMode = SpriteSortMode.Immediate, SamplerState = SamplerState.PointWrap });
-        
-        var device = Main.graphics.GraphicsDevice;
-        device.RasterizerState = RasterizerState.CullNone;
-        
-        var shader = Data.Instance.FlameShader;
-
-        var vertexBuffer = Data.Instance.VertexBuffer;
-        var indexBuffer = Data.Instance.IndexBuffer;
-        vertexBuffer.SetData(vertices.ToArray(), 0, vertices.Count);
-        indexBuffer.SetData(indices.ToArray(), 0, indices.Count);
-        device.SetVertexBuffer(vertexBuffer);
-        device.Indices = indexBuffer;
-            
-        device.Textures[0] = texture;
-        device.Textures[1] = textureNoise;
-        shader.Apply();
-        device.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0, vertices.Count, 0, indices.Count / 3);
         
         sb.Restart(in ss);
     }
