856a389e043e4c000812e777930485015617388b
yoyocattus <may0naisecattus@gmail.com>
2026-08-22 13:30:29 +0100

    new rubbles

--- src/Solstice/Content/Aerie/Placements/GravelRubbles.cs
+++ src/Solstice/Content/Aerie/Placements/GravelRubbles.cs
@@ -7,6 +7,27 @@ using Terraria.ObjectData;
 
 namespace Solstice.Content.Aerie;
 
+public abstract class GravelPebble : ModTile
+{
+    public override string Texture => Assets.Images.Aerie.Placements.GravelPebble.KEY;
+    
+    public override void SetStaticDefaults()
+    {
+        Main.tileFrameImportant[Type] = true;
+        Main.tileNoFail[Type] = true;
+
+        DustType = DustID.Stone;
+        Main.tileMerge[ModContent.TileType<AerieGravelTile>()][Type] = true;
+        TileObjectData.newTile.CopyFrom(TileObjectData.Style1x1);
+        TileObjectData.newTile.StyleHorizontal = true;  
+        TileObjectData.addTile(Type);
+        
+        AddMapEntry(new Color(122, 116, 102));
+    }
+    
+    public override void NumDust(int i, int j, bool fail, ref int num) => num = 1;
+}
+
 public abstract class GravelPile : ModTile
 {
     public override string Texture => Assets.Images.Aerie.Placements.GravelPile.KEY;
@@ -24,6 +45,8 @@ public abstract class GravelPile : ModTile
         
         AddMapEntry(new Color(88, 83, 65));
     }
+    
+    public override void NumDust(int i, int j, bool fail, ref int num) => num = 3;
 }
 
 public abstract class GravelDebris : ModTile
@@ -42,6 +65,19 @@ public abstract class GravelDebris : ModTile
         TileObjectData.addTile(Type);
         
         AddMapEntry(new Color(88, 83, 65));
+    }
+    
+    public override void NumDust(int i, int j, bool fail, ref int num) => num = 3;
+}
+
+public class GravelPebbleEcho : GravelPebble
+{
+    public override void SetStaticDefaults() {
+        base.SetStaticDefaults();
+        
+        FlexibleTileWand.RubblePlacementSmall.AddVariations(ModContent.ItemType<AerieGravel>(), Type, 0, 1, 2, 3);
+        
+        RegisterItemDrop(ModContent.ItemType<AerieGravel>());
     }
 }
 
